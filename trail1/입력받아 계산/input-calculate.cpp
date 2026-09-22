e7f57da42ede70bb26e6d70c2688999c0442e040
BBJ0110 <eeub1290@gmail.com>
2026-09-22 10:28:51 +0900

    [Passed] 입력받아 계산 (2ms, 0MB)

---
+++ trail1/입력받아 계산/input-calculate.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int input;
+    std::cin >> input;
+    std::cout << input + 2;
+    return 0;
+}
