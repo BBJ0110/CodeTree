deeeb49ed17a1a1b07bfae21eac3f7f36b47ba62
BBJ0110 <eeub1290@gmail.com>
2026-09-22 10:29:57 +0900

    [Passed] 점수 입출력 (2ms, 0MB)

---
+++ trail1/점수 입출력/enter-int-and-print-score.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int input;
+    std::cin >> input;
+    std::cout << "Your score is " << input << " point."; 
+    return 0;
+}
